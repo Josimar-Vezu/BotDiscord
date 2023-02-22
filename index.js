02f4003577afa7a907b6664586a030a61565f061
Josimar-Vezu <josimarvezuramires@gmail.com>
2023-02-21 23:27:16 -0300

    Criação de Comandos

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
 
 // dotenv
 const dotenv = require('dotenv')
@@ -12,18 +12,24 @@ const path = require("node:path")
 const commandsPath = path.join(__dirname, "commands")
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"))
 
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.commands = new Collection()
+
 for (const file of commandFiles) {
 	const filePath = path.join(commandsPath, file)
 	const command = require(filePath)
+	console.log(command)
 	if ("data" in command && "execute" in command) {
-
+		client.commands.set(command.data.name, command)
+	} else {
+		console.log(`Esse comando em ${filePath} está com "data" ou "execute" ausentes`)
 	}
 }
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+console.log(client.commands)
 
 client.once(Events.ClientReady, c => {
 	console.log(`Pronto! Login Realizado como ${c.user.tag}`)
 });
 
-client.login(TOKEN);
+client.login(TOKEN)
