b9a7bc59fd44026955969ae63b9e7a44a72b16b9
Josimar-Vezu <josimarvezuramires@gmail.com>
2023-02-22 19:47:54 -0300

    Finalização do Discord

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,15 +1,13 @@
-const { REST, Routes } = require('discord.js')
+const { REST, Routes } = require("discord.js")
 
 // dotenv
 const dotenv = require('dotenv')
 dotenv.config()
-const { TOKEN, CLIENTE_ID, GUILD_ID } = process.env
+const { TOKEN, CLIENT_ID, GUILD_ID } = process.env
 
 // importação dos comandos
 const fs = require("node:fs")
 const path = require("node:path")
-const { CLIENT_RENEG_WINDOW } = require('node:tls')
-
 const commandsPath = path.join(__dirname, "commands")
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"))
 
@@ -19,19 +17,21 @@ for (const file of commandFiles) {
     const command = require(`./commands/${file}`)
     commands.push(command.data.toJSON())
 }
-//INSTÂNCIA Rest
+
+// instância REST
 const rest = new REST({ version: "10" }).setToken(TOKEN);
 
-//deploy
+// deploy
 (async () => {
     try {
-        console.log(`Resetando ${commands.length} comandos...`)
-        //put
+        console.log(`Resentando ${commands.length} comandos...`)
+
+        // PUT
         const data = await rest.put(
-            Routes.applicationGuildCommands(CLIENTE_ID, GUILD_ID),
+            Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
             { body: commands }
         )
-        console.log("Comandos Registrados com Sucesso")
+        console.log("Comandos registrados com sucesso!")
     }
     catch (error) {
         console.error(error)
