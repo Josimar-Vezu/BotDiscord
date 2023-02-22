29b59acb558f0e741cd141dac615d75ec5e99510
Josimar-Vezu <josimarvezuramires@gmail.com>
2023-02-22 17:38:51 -0300

    Direcionando os comandos para pasta e validando

--- deploy-commands.js
+++ deploy-commands.js
@@ -3,4 +3,37 @@ const { REST, Routes } = require('discord.js')
 // dotenv
 const dotenv = require('dotenv')
 dotenv.config()
-const { TOKEN } = process.env
+const { TOKEN, CLIENTE_ID, GUILD_ID } = process.env
+
+// importação dos comandos
+const fs = require("node:fs")
+const path = require("node:path")
+const { CLIENT_RENEG_WINDOW } = require('node:tls')
+
+const commandsPath = path.join(__dirname, "commands")
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"))
+
+const commands = []
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`)
+    commands.push(command.data.toJSON())
+}
+//INSTÂNCIA Rest
+const rest = new REST({ version: "10" }).setToken(TOKEN);
+
+//deploy
+(async () => {
+    try {
+        console.log(`Resetando ${commands.length} comandos...`)
+        //put
+        const data = await rest.put(
+            Routes.applicationGuildCommands(CLIENTE_ID, GUILD_ID),
+            { body: commands }
+        )
+        console.log("Comandos Registrados com Sucesso")
+    }
+    catch (error) {
+        console.error(error)
+    }
+})()
--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
 
-// importação dos comandos
-const fs = require("node:fs")
-const path = require("node:path")
-
-const commandsPath = path.join(__dirname, "commands")
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"))
-
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 client.commands = new Collection()
 
